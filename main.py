7bb898b81abc3d3e5db5d8f73db978f7731fa835
dunee22 <dunee.29@gmail.com>
2026-06-01 17:01:26 -0600

    Add downtime vs defects correlation analysis

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 df = pd.read_csv("data/production_data.csv") # Asegúrate de que la ruta al archivo CSV sea correcta. Si el archivo está en el mismo directorio que tu script, puedes usar simplemente "production_data.csv". Si está en una carpeta llamada "data", entonces la ruta es "data/production_data.csv".
 print(df)
@@ -43,3 +44,47 @@ plt.ylim(0,1600)
 for day, production in zip(df["Day"], df["Production"]): 
     plt.text(day, production + 15, str(production),ha="center") # Esto agrega etiquetas de texto encima de cada barra en el gráfico, mostrando el valor de producción correspondiente a cada día. El texto se coloca un poco por encima de la barra (production + 15) para que sea visible.
 plt.show()
+
+plt.bar(
+    df["Day"],
+    df["Defects"],
+    color=["darkcyan","darkcyan","darkcyan","darkcyan","darkgreen","crimson","darkcyan"]
+    ) # Esto crea un gráfico de barras utilizando la columna "Day" para el eje x y la columna "Defects" para el eje y.
+plt.title("Defects by Day")
+plt.xlabel("Day")
+plt.ylabel("Defects")
+plt.ylim(0,100)
+for day, defects in zip(df["Day"], df["Defects"]): 
+    plt.text(day, defects +1, str(defects),ha="center") # Esto agrega etiquetas de texto encima de cada barra en el gráfico, mostrando el valor de defectos correspondiente a cada día. El texto se coloca un poco por encima de la barra (defects + 15) para que sea visible.
+plt.show()
+
+plt.scatter(df["Downtime_Minutes"], df["Defects"], color="darkcyan", s=100, alpha=0.8,) # Esto crea un gráfico de dispersión utilizando la columna "Downtime_minutes" para el eje x y la columna "Defects" para el eje y. Cada punto en el gráfico representa un día específico, con su tiempo de inactividad en minutos y el número de defectos correspondientes.
+plt.grid(alpha=0.3)
+plt.xlim(5,75)
+plt.ylim(15,65)
+plt.title("Defects vs Downtime")
+plt.xlabel("Downtime (minutes)")
+plt.ylabel("Defects")
+plt.show()
+
+pendiente, intercepto = np.polyfit(
+    df["Downtime_Minutes"],
+    df["Defects"],
+    1
+)
+
+y_linea = pendiente * df["Downtime_Minutes"] + intercepto
+
+plt.scatter(df["Downtime_Minutes"], df["Defects"], color="darkcyan", s=100, alpha=0.8,label="Actual data") # Esto crea un gráfico de dispersión utilizando la columna "Downtime_minutes" para el eje x y la columna "Defects" para el eje y. Cada punto en el gráfico representa un día específico, con su tiempo de inactividad en minutos y el número de defectos correspondientes.
+
+plt.plot(
+    df["Downtime_Minutes"],
+    y_linea,
+    color="crimson",
+    linewidth=3,
+    label="Trend Line"
+    
+)
+plt.legend()
+plt.show()
+# Esto realiza un ajuste de línea recta (grado 1) a los datos de "Downtime_minutes" y "Defects". Devuelve los coeficientes de la línea ajustada, donde el primer valor es la pendiente y el segundo valor es la intersección con el eje y. Estos coeficientes se pueden usar para trazar la línea de tendencia en el gráfico de dispersión.
